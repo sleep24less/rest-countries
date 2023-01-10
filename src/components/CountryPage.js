4bab0642a9065fb7fe49d34d25b86bb75ae0fe65
sleepless <arnas.manucan@gmail.com>
2023-01-10 11:39:10 +0200

    Fix country page border countries
    Now they display as a full name, not a shorthand

--- src/components/CountryPage.js
+++ src/components/CountryPage.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import { motion } from 'framer-motion'
 import { Link, useParams } from 'react-router-dom';
-import axios from 'axios';
+import axios, { Axios } from 'axios';
 import '../countrypage.css'
 
 function CountryPage() {
     const [country, setCountry] = React.useState([]);
+    const [bordersData, setBordersData] = React.useState([]);
     const { name } = useParams();
     const url = `https://restcountries.com/v2/name/${name}`;
+    let arrayBorders = [];
 
-    const getCountryData = async () => {
+    React.useEffect(() => {
+        getCountryData(url);
+    }, [url])
+
+    const getCountryData = async (url) => {
         const data = await axios.get(url).then(res => res.data);
         setCountry(data);
     }
 
+
     React.useEffect(() => {
-        getCountryData();
-    })
+        if (country[0]?.borders) {
+            country[0].borders.forEach(async border => {
+                const borderUrl = `https://restcountries.com/v2/alpha/${border}`
+                const res = await axios.get(borderUrl)
+                setBordersData(bordersData => [...bordersData, res.data]);
+            });
+        }
+    }, [country])
 
     return (
         <motion.div
@@ -33,7 +46,6 @@ function CountryPage() {
 
                 let stringCurrencies = [];
                 let stringLanguages = [];
-                let arrayBorders = [];
                 if (currencies) {
                     stringCurrencies = currencies.map(c => `${c.name}`).join(', ') + '.';
                 }
@@ -49,23 +61,27 @@ function CountryPage() {
                         <img className='flag' src={flag} alt={`Flag of ${name}`}></img>
                         <div className='info_section'>
                             <h1 className='name'>{name}</h1>
-                            <ul className='info'>
+                            <ul className='info_left'>
                                 <li className='info_item'>Native Name: <span>{nativeName}</span></li>
                                 <li className='info_item'>Population: <span>{population}</span></li>
                                 <li className='info_item'>Region: <span>{region}
                                 </span></li>
                                 <li className='info_item'>Sub Region: <span>{subregion}</span></li>
                                 {capital && <li className='info_item capital'>Capital: <span>{capital}</span></li>}
+                            </ul>
+                            <ul className='info_right'>
                                 <li className='info_item'>Top Level Domain: <span>{topLevelDomain}</span></li>
                                 {currencies && <li className='info_item'>Currencies: <span>{stringCurrencies}</span></li>}
                                 <li className='info_item'>Languages: <span>{stringLanguages}</span></li>
                             </ul>
-                            {borders && <ul className='border'>
+                            {bordersData.length > 0 && <ul className='border'>
                                 Border Countries:
-                                {arrayBorders.map(element => {
-                                    return (
-                                        <li className='border_item'>{element}</li>
-                                    )
+                                {bordersData.map((borderCountry) => {
+                                    if (arrayBorders.includes(borderCountry.alpha3Code)) {
+                                        return (
+                                            <li key={borderCountry.alpha3Code} className='border_item'>{borderCountry.name}</li>
+                                        )
+                                    }
                                 })}
                             </ul>}
                         </div>
